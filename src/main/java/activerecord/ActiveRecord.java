b795734ae1116b79331a4cca7c2d491ddf3f683c
Guillaume Wallet <wallet.guillaume@gmail.com>
2012-07-01 20:50:20 +0200

    Adding some JavaDoc comment

--- src/main/java/activerecord/ActiveRecord.java
+++ src/main/java/activerecord/ActiveRecord.java
@@ -13,6 +13,57 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The main purpose of this class is to managed the database relationship of sub-classes instances.
+ *
+ * <p>Active record objects are instances of classes declared like this:</p>
+ * <pre>
+ * public class Contact
+ *     extends ActiveRecord&lt;Contact>
+ * {
+ *     private String firstName;
+ *     private String lastName;
+ *     private String email;
+ * }
+ * </pre>
+ * <p>
+ *     In this way, you can {@linkplain #save(javax.sql.DataSource) insert/update},
+ *     {@linkplain #find(javax.sql.DataSource) find} and
+ *     {@linkplain #delete(javax.sql.DataSource) delete} rows in database with the inherited methods.
+ * </p>
+ * <h2>Create / Update</h2>
+ * To create or update, simply create an instance like anyone in Java, then store it in database like this :
+ * <pre>
+ * Contact contact = new Contact();
+ * // ...
+ * // populate fields
+ * // ...
+ * contact.save( dataSource );
+ * </pre>
+ * The database is now up to date or contains a new row.
+ * <h2>Find</h2>
+ * To find instances, simply create a candidate instance with some required field and then find equivalent in database like this :
+ * <pre>
+ * Contact candidate = new Contact();
+ * // ...
+ * // populate matching fields
+ * // ...
+ * List&lt;Contact> contacts = candidate.find( dataSource );
+ * </pre>
+ * The list <code>contacts</code> contains all found contact in database corresponding to the given <code>candidate</code>.
+ * <h2>Delete</h2>
+ * To delete a row, simply create a corresponding candidate and then delete it like this :
+ * <pre>
+ * Contact candidate = new Contact();
+ * // ...
+ * // populate matching fields
+ * // ...
+ * candidate.delete( dataSource );
+ * </pre>
+ *
+ * @see <a href="http://en.wikipedia.org/wiki/Active_record_pattern">Active record design pattern</a>
+ * @param <T> Type of managed active record.
+ */
 public abstract class ActiveRecord<T extends ActiveRecord>
 {
     @SuppressWarnings("unchecked")
@@ -24,6 +75,11 @@ public abstract class ActiveRecord<T extends ActiveRecord>
         return LoggerFactory.getLogger(getClass());
     }
 
+    /**
+     * Save this instance to the target database.
+     * @param dataSource Previously configured target database connection.
+     * @throws SQLException This may failed, sorry.
+     */
     public void save( DataSource dataSource )
         throws SQLException
     {
@@ -65,6 +121,13 @@ public abstract class ActiveRecord<T extends ActiveRecord>
         }
     }
 
+    /**
+     * Find all corresponding rows corresponding to this one in the target database.
+     * @param dataSource Previously configured target database connection.
+     * @return Return a list containing all the corresponding instance found in database. If no corresponding instance can
+     * be found, an empty list is return.
+     * @throws SQLException This may failed, sorry.
+     */
     public List<T> find( DataSource dataSource )
         throws SQLException
     {
@@ -77,7 +140,7 @@ public abstract class ActiveRecord<T extends ActiveRecord>
                 stopwatch.reset().start();
                 try (ResultSet resultSet = statement.executeQuery()) {
                     stopwatch.stop();
-                    logger().info( "Executed query '{}' with values {} in {} ms", new Object []{query, args, stopwatch.elapsedMillis()});
+                    logger().info( "Executed query '{}' with values {} in {} ms", new Object []{ query, args, stopwatch.elapsedMillis() } );
                     ArrayList<T> results = new ArrayList<>();
                     createResultsFromResultSet( resultSet, results );
                     return results;
@@ -90,18 +153,30 @@ public abstract class ActiveRecord<T extends ActiveRecord>
         throws SQLException
     {
         while ( resultSet.next() ) {
-            try {
-                T instance = clazz.newInstance();
-                int index = 1;
-                for (Field field : clazz.getDeclaredFields() ) {
-                    field.setAccessible(true);
-                    field.set( instance, resultSet.getObject( index++ ) );
-                }
-                results.add( instance );
-            } catch ( IllegalAccessException|InstantiationException cause ) {
-                throw new RuntimeException("Unable to execute selection query", cause);
-            }
+            results.add( createResultFromRow( resultSet ) );
         }
+    }
+
+    private T createResultFromRow( ResultSet resultSet )
+        throws SQLException
+    {
+        try {
+            return createResultFromRowWithError( resultSet );
+        } catch ( IllegalAccessException|InstantiationException cause ) {
+            throw new RuntimeException("Unable to execute selection query", cause);
+        }
+    }
+
+    private T createResultFromRowWithError( ResultSet resultSet )
+        throws SQLException, IllegalAccessException, InstantiationException
+    {
+        T instance = clazz.newInstance();
+        int index = 1;
+        for ( Field field : clazz.getDeclaredFields() ) {
+            field.setAccessible( true );
+            field.set( instance, resultSet.getObject( index++ ) );
+        }
+        return instance;
     }
 
     private String buildSelectionQuery( ArrayList<Object> args )
@@ -135,6 +210,11 @@ public abstract class ActiveRecord<T extends ActiveRecord>
         return select.from(clazz.getSimpleName()).toString() + whereClause.toString();
     }
 
+    /**
+     * Delete this instance from the target database.
+     * @param dataSource Previously configured target database connection.
+     * @throws SQLException This may failed, sorry.
+     */
     public void delete( DataSource dataSource )
         throws SQLException
     {
